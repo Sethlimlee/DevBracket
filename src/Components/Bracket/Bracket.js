4ba9d44dedce4b03b71ac70db87cead33b8d2212
SethLee <sethlimlee@gmail.com>
2018-04-05 16:18:13 -0600

    non scalable

--- src/Components/Bracket/Bracket.js
+++ src/Components/Bracket/Bracket.js
@@ -1,4 +1,7 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { getUserInfo } from "../../ducks/users";
+import axios from 'axios';
 import "./bracket.css";
 
 class Bracket extends Component {
@@ -12,106 +15,131 @@ class Bracket extends Component {
         { name: "Steven", wins: 0, position: 4 }
       ]
     };
+  }
+
+  componentDidMount() {
+    this.props.getUserInfo();
     
-  }
-  winGameP1(){
+    
+    // axios.get(`/api/team/${this.props.match.params.team_name}`).then(res => {
+    //   console.log(res)
+    //   this.setState({
+    //     team: res.data
+    //   })
+    // })
 
+  }
+
+  winGameP1() {
     this.setState({
-      users: [...this.state.users, this.state.users[0].wins += 1]
-        })
-        console.log(this.state.users[0])
+      users: [...this.state.users, (this.state.users[0].wins += 1)]
+    });
+    console.log(this.state.users[0]);
   }
 
-  winGameP2(){
-
+  winGameP2() {
     this.setState({
-      users: [...this.state.users, this.state.users[1].wins += 1]
-        })
-        console.log(this.state.users[1])
+      users: [...this.state.users, (this.state.users[1].wins += 1)]
+    });
+    console.log(this.state.users[1]);
   }
 
-  winGameP3(){
-
+  winGameP3() {
     this.setState({
-      users: [...this.state.users, this.state.users[2].wins += 1]
-        })
-        console.log(this.state.users[2])
+      users: [...this.state.users, (this.state.users[2].wins += 1)]
+    });
+    console.log(this.state.users[2]);
   }
 
-  winGameP4(){
+  winGameP4() {
     this.setState({
-      users: [...this.state.users, this.state.users[3].wins = 1]
-        })
-        
-        console.log(this.state.users[3])
-        console.log(this.state)
-  }
+      users: [...this.state.users, (this.state.users[3].wins = 1)]
+    });
 
+    console.log(this.state.users[3]);
+    console.log(this.state);
+  }
 
   render() {
-    console.log(this.state)
-    const p1 = this.state.users.map(player => {
-      return (
-        <div key={player.name}>
-          {player.position === 1 ? <p>{player.name}</p> : null}
-        </div>
-      );
-    });
+    const p1 = this.state.users
+      .filter(player => player.position === 1)
+      .map(player => {
+        return (
+          <div key={player.name}>
+            <p>{player.name}</p>
+          </div>
+        );
+      });
+    const p2 = this.state.users
+      .filter(player => player.position === 2)
+      .map(player => {
+        return (
+          <div key={player.name}>
+            <p>{player.name}</p>
+          </div>
+        );
+      });
 
-    const p2 = this.state.users.map(player => {
-      return (
-        <div key={player.name}>
-          {player.position === 2 ? <p>{player.name}</p> : null}
-        </div>
-      );
-    });
+    const p3 = this.state.users
+      .filter(player => player.position === 3)
+      .map(player => {
+        return (
+          <div key={player.name}>
+            <p>{player.name}</p>
+          </div>
+        );
+      });
 
-    const p3 = this.state.users.map(player => {
-      return (
-        <div key={player.name}>
-          {player.position === 3 ? <p>{player.name}</p> : null}
-        </div>
-      );
-    });
+    const p4 = this.state.users
+      .filter(player => player.position === 4)
+      .map(player => {
+        return (
+          <div key={player.name}>
+            <p>{player.name}</p>
+          </div>
+        );
+      });
 
-    const p4 = this.state.users.map(player => {
-      return (
-        <div key={player.name}>
-          {player.position === 4 ? <p>{player.name}</p> : null}
-        </div>
-      );
-    });
+    const winner1 = this.state.users
+      .filter(
+        player =>
+          (player.wins !== 0) & (player.position === 3 || player.position === 4)
+      )
+      .map(player => {
+        return (
+          <div key={player.name}>
+            <p>{player.name}</p>
+          </div>
+        );
+      });
 
-    const winner1 = this.state.users.map(player => {
-      return (
-        <div key={player.position}>
-          {(player.wins !== 0) & (player.position === 3 || player.position === 4)  ? <p>{player.name}</p> : null}
-        </div>
-      );
-    });
+    const winner2 = this.state.users
+      .filter(
+        player =>
+          (player.wins !== 0) & (player.position === 1 || player.position === 2)
+      )
+      .map(player => {
+        return (
+          <div key={player.name}>
+            <p>{player.name}</p>
+          </div>
+        );
+      });
 
-    const winner2 = this.state.users.map(player => {
-      return (
-        <div key={player.position}>
-          {player.wins !== 0 & (player.position === 1 || player.position === 2)  ? <p>{player.name}</p> : null}
-        </div>
-      );
-    });
-
-    // console.log(winner1.length)
+    console.log(winner1.length)
     // console.log(this.state.users.length);
     // console.log(this.state)
-    
+
     // console.log(winner1)
     return (
       <div className="bracket">
         <div className="box1">
-        <div className="buttons">
+          <div className="buttons">
             <div>
-              <button onClick={() => this.winGameP1()}>win</button>
+              <button onClick={winner2.length < 1 ? () => this.winGameP1() : null}>win</button>
             </div>
             <div>
-              <button onClick={() => this.winGameP2()}>win</button>
+              <button onClick={winner2.length < 1 ? () => this.winGameP2() : null}>win</button>
             </div>
           </div>
 
@@ -121,16 +149,16 @@ class Bracket extends Component {
           </div>
         </div>
         <div className="box3">
-        {winner1}
-        {winner2}
+          {winner1}
+          {winner2}
         </div>
         <div className="box2">
           <div className="buttons">
             <div>
-              <button onClick={() => this.winGameP3()}>win</button>
+              <button onClick={winner1.length < 1 ? () => this.winGameP3() : null}>win</button>
             </div>
             <div>
-              <button onClick={() => this.winGameP4()}>win</button>
+              <button onClick={winner1.length < 1 ? () => this.winGameP4() : null}>win</button>
             </div>
           </div>
 
@@ -143,4 +171,12 @@ class Bracket extends Component {
     );
   }
 }
-export default Bracket;
+function mapStateToProps(state) {
+  console.log(state);
+  return {
+    user: state.user
+  };
+}
+
+export default connect(mapStateToProps, { getUserInfo })(Bracket);
+
