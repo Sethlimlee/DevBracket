6e2b081b49db8d41c5e71bcd7fd1da9383942e61
SethLee <sethlimlee@gmail.com>
2018-04-19 10:14:16 -0600

    right before host

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,32 +1,106 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import "./nav.css";
 
-function Nav() {
-  return (
-    <div className="nav">
+class Nav extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toggle: false
+    };
+  }
+  myFunction() {
+    this.setState({
+      toggle: !this.state.toggle
+    });
+  }
+
+  render() {
+    console.log(this.state.toggle)
+    return (
       <div>
-        <Link className='home' to="/home">D E V B R A C K E T</Link>
+        {this.state.toggle === true 
+        ? 
+        <div className="mobilenav">
+        
+       
+          <div>
+            <Link onClick={() => this.myFunction()} className="MobileLinks" to="/bracketcreator">
+              Bracket Creator
+            </Link>
+          </div>
+          <div>
+            <Link onClick={() => this.myFunction()} className="MobileLinks" to="/joinbrackets">
+              Join Bracket
+            </Link>
+          </div>
+          <div>
+            <Link onClick={() => this.myFunction()} className="MobileLinks" to="/allbrackets">
+              All Brackets
+            </Link>
+          </div>
+          <div>
+            <Link onClick={() => this.myFunction()} className="MobileLinks" to="/rankings">
+              Rankings
+            </Link>
+          </div>
+          <div>
+            <a onClick={() => this.myFunction()} className="logoutmobile" href="http://localhost:3005/login/logout">
+              <p>Logout</p>
+            </a>
+          </div>
+        
+        
+        </div>
+        : ""}
+        <div className="nav">
+          <div>
+            <Link className="home" to="/home">
+              D E V B R A C K E T
+            </Link>
+          </div>
+          <div>
+            <Link className="navlinks" to="/bracketcreator">
+              Bracket Creator
+            </Link>
+          </div>
+          <div>
+            <Link className="navlinks" to="/joinbrackets">
+              Join Bracket
+            </Link>
+          </div>
+          <div>
+            <Link className="navlinks" to="/allbrackets">
+              All Brackets
+            </Link>
+          </div>
+          <div>
+            <Link className="navlinks" to="/rankings">
+              Rankings
+            </Link>
+          </div>
+          <div>
+            <a className="logout" href="http://localhost:3005/login/logout">
+              <p>Logout</p>
+            </a>
+          </div>
+
+          <div onClick={() => this.myFunction()}>
+            <svg
+              className="menu-svg"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 50 50"
+            >
+              <path
+                fill="#fff"
+                d="M8.667 15h30c.552 0 1-.447 1-1s-.448-1-1-1h-30c-.552 0-1 .447-1 1s.447 1 1 1zM8.667 37h30c.552 0 1-.447 1-1s-.448-1-1-1h-30c-.552 0-1 .447-1 1s.447 1 1 1zM8.667 26h30c.552 0 1-.447 1-1s-.448-1-1-1h-30c-.552 0-1 .447-1 1s.447 1 1 1z"
+              />
+            </svg>
+          </div>
+        </div>
       </div>
-      <div>
-        <Link className='navlinks' to="/bracketcreator">Bracket Creator</Link>
-      </div>
-      <div>
-        <Link className='navlinks' to="/joinbrackets">Join Bracket</Link>
-      </div>
-      <div>
-        <Link className='navlinks' to="/allbrackets">All Brackets</Link>
-      </div>
-      <div>
-        <Link className='navlinks' to="/rankings">Rankings</Link>
-      </div>
-      <div>
-        <a className='logout' href="http://localhost:3005/login/logout">
-          <p>Logout</p>
-        </a>
-      </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Nav;
