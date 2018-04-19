8c27102b5cc2d425d759864fd282471f50e41a3b
SethLee <sethlimlee@gmail.com>
2018-04-19 16:50:53 -0600

    allbrackets change

--- src/Components/AllBrackets/AllBrackets.js
+++ src/Components/AllBrackets/AllBrackets.js
@@ -34,7 +34,7 @@ class AllBrackets extends Component {
     : 'log in first'
     return (
       <div className='outsidebox'>
-        <div className='profileFind'>
+        <div className='profile'>
           <div className='allbrackets'>
             <div className = 'allbracketstitle'>Brackets:</div>
             <div className='displayallbrackets'>{displayBrackets}</div>
